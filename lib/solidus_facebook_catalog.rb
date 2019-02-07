448a1d559a798fc43ffba979131ab49edd5b442f
Noel <noel@2bedigital.com>
2019-02-07 16:18:21 +0000

    fixes in require lib

--- lib/solidus_facebook_catalog.rb
+++ lib/solidus_facebook_catalog.rb
@@ -1,6 +1,9 @@
 require "solidus_facebook_catalog/version"
 require "solidus_core"
-require 'sales' if defined?(Sales)
+begin
+  require 'sales'
+rescue LoadError
+end
 require "solidus_facebook_catalog/engine"
 require "solidus_facebook_catalog/configure"
 require "solidus_facebook_catalog/product_catalog"
